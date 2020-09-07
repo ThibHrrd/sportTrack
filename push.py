1c70b534c65410f947694128e834fc8822161093
Leo <leo951206@gmail.com>
2020-09-07 02:47:46 -0700

    test

---
+++ push.py
@@ -0,0 +1,9 @@
+import os,sys
+
+if len(sys.argv) != 2:
+	print("1 argument required")
+else:
+	os.system("git pull")
+	os.system("git add * ")
+	os.system("git commit -m "+sys.argv[1])
+	os.system("git push")
