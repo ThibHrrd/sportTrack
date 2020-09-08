21d1da18061b8c349039fa8d1e674c26f567bee8
Tinlayer <tinlayer@gmail.com>
2020-09-08 09:12:00 +0200

    BDD creation MAJ

--- bdd/create_db.sql
+++ bdd/create_db.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS user (
 
+	id_user INTEGER(4) PRIMARY KEY,
 	lastname TEXT NOT NULL,
 	firstname TEXT NOT NULL,
 	birthday DATE NOT NULL,
@@ -7,24 +8,28 @@ CREATE TABLE IF NOT EXISTS user (
 	weight INTEGER NOT NULL,
 	height INTEGER NOT NULL,
 	mail TEXT NOT NULL,
-	password TEXT NOT NULL,
-
-	PRIMARY KEY (lastname, firstname)
+	password TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS activity (
 
+	id_activity INTEGER(4) PRIMARY KEY,
 	activity_date TIMESTAMP NOT NULL,
 	activity_description TEXT NOT NULL,
 
-	PRIMARY KEY (activity_date, activity_description)
+	aUser INTEGER(4) NOT NULL,
+	FOREIGN KEY(aUser) REFERENCES user(id_user)
 );
 
 CREATE TABLE IF NOT EXISTS data_acitivty (
 
+	id_data INTEGER(4) PRIMARY KEY,
 	data_time DATE TIME NOT NULL,
 	cardio_frequency INTEGER NOT NULL,
 	latitude FLOAT NOT NULL,
 	longitude FLOAT NOT NULL,
-	altitude INTEGER NOT NULL
+	altitude INTEGER NOT NULL,
+
+	anActivity INTEGER(4) NOT NULL,
+	FOREIGN KEY(anActivity) REFERENCES activity(id_activity)
 );
