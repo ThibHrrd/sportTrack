489f56470dba598fe06a7d689d1e9e80bf6d8281
Tinlayer <tinlayer@gmail.com>
2020-09-08 11:18:34 +0200

    BDD create MAJ

--- bdd/create_db.sql
+++ bdd/create_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user (
 
-	id_user INTEGER(4) PRIMARY KEY,
+	id_user INTEGER PRIMARY KEY,
 	lastname TEXT NOT NULL,
 	firstname TEXT NOT NULL,
 	birthday DATE NOT NULL,
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS user (
 
 CREATE TABLE IF NOT EXISTS activity (
 
-	id_activity INTEGER(4) PRIMARY KEY,
+	id_activity INTEGER PRIMARY KEY,
 	activity_date TIMESTAMP NOT NULL,
 	activity_description TEXT NOT NULL,
 
-	aUser INTEGER(4) NOT NULL,
+	aUser INTEGER NOT NULL,
 	FOREIGN KEY(aUser) REFERENCES user(id_user)
 );
 
@@ -30,11 +30,14 @@ CREATE TABLE IF NOT EXISTS data_acitivty (
 	longitude FLOAT NOT NULL,
 	altitude INTEGER NOT NULL,
 
-	anActivity INTEGER(4) NOT NULL,
+	anActivity INTEGER NOT NULL,
 	FOREIGN KEY(anActivity) REFERENCES activity(id_activity)
 );
 
+DELETE FROM user;
+DELETE FROM activity;
+DELETE FROM data_acitivty;
 
-INSERT INTO user VALUES(0001, "HERARD", "Thibault", datetime('now'), 0, 180, 70, "thibz@gmail.com", "ouioui");
-INSERT INTO activity VALUES(0001, datetime('now'), "course de 800m", 0001);
-INSERT INTO data_acitivty VALUES(0001, datetime('localtime'), 90, 47.12, -2.123, 18, 0001);
+INSERT INTO user VALUES(0001, 'HERARD', 'Thibault', datetime('now'), 0, 180, 70, 'thibz@gmail.com', 'ouioui');
+INSERT INTO activity VALUES(0002, datetime('now'), 'course de 800m', 0001);
+INSERT INTO data_acitivty VALUES(0003, datetime('now'), 90, 47.12, -2.123, 18, 0002);
