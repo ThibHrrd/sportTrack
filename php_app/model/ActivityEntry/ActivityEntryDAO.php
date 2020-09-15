15f13a93a1185c448c6d088ea21717f9f58fe06c
Leo <leo951206@gmail.com>
2020-09-15 01:28:42 -0700

    ActivityEntryDAO

---
+++ php_app/model/ActivityEntry/ActivityEntryDAO.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once (__DIR__."/../SqliteConnection.php");
+include("Activity.php");
+
+class ActivityEntryDAO {
+
+    private static $dao;
+
+    public final static function getInstance() {
+
+        if(!isset(self::$dao)) {         //On verifie que l'objet est créer.
+            self::$dao= new ActivityEntryDAO();   //Si il est pas créer, alors on créer un unique objet DAO.
+        }
+        return self::$dao;
+    }
+
+    public final function findAll(){
+        $dbc = SqliteConnection::getInstance()->getConnection();
+        $query = "select * from data_activity order by data_time";
+        $stmt = $dbc->query($query);
+        $results = $stmt->fetchALL(PDO::FETCH_CLASS, 'Data');
+        return $results;
+    }
+
+    public final function insert($d){
+
+        if($d instanceof Data){
+
+           $dbc = SqliteConnection::getInstance()->getConnection();
+           // prepare the SQL statement
+           $query = "insert into data_activity(id_data, data_time, cardio_frequency, latitude, longitude, altitude, anActivity) values (:i,:d,:c,:la,:lo,:a,:act)";
+           $stmt = $dbc->prepare($query);
+  
+           // bind the paramaters
+           $stmt->bindValue(':i',$d->getID(),PDO::PARAM_INT);
+           $stmt->bindValue(':d',$d->getDataTime(),PDO::PARAM_STR);
+           $stmt->bindValue(':c',$d->getCardioFrequency(),PDO::PARAM_STR);
+           $stmt->bindValue(':la',$d->getLatitude(),PDO::PARAM_STR);
+           $stmt->bindValue(':lo',$d->getLongitude(),PDO::PARAM_STR);
+           $stmt->bindValue(':a',$d->getAltitude(),PDO::PARAM_STR);
+           $stmt->bindValue(':act',$d->getActivity(),PDO::PARAM_STR);
+
+           
+
+           // execute the prepared statement
+           $stmt->execute();
+       }
+    }
+
+    public function delete($d){
+
+        if($d instanceof Data){
+
+            $dbc = SqliteConnection::getInstance()->getConnection();
+            // prepare the SQL statement
+            $query = "delete from data_activity WHERE (id_data= :i)";
+            $stmt = $dbc->prepare($query);
+  
+            // bind the paramaters
+            $stmt->bindValue(':i',$d->getID(),PDO::PARAM_INT);
+
+            // execute the prepared statement
+            $stmt->execute();
+
+        }
+    }
+
+    public function deleteAll(){
+
+        $dbc = SqliteConnection::getInstance()->getConnection();
+        // prepare the SQL statement
+        $query = "delete from data_activity";
+        $stmt = $dbc->prepare($query);
+
+        // execute the prepared statement
+        $stmt->execute();
+
+    }
+
+    public function update($d){
+
+        if($d instanceof Data){
+    
+            $this->delete($d); //It's a subterfuge :)
+            $this->insert($d); 
+    
+        }
+    }
+
+    }
--- php_app/model/sqlite_connection_test.php
+++ php_app/model/sqlite_connection_test.php
@@ -1,8 +1,10 @@
 <?php
 
 // include the DAO file
-include("User/UserDAO.php");
-include("Activity/ActivityDAO.php");
+include_once("User/UserDAO.php");
+include_once("Activity/ActivityDAO.php");
+include_once("ActivityEntry/ActivityEntryDAO.php");
+include_once("ActivityEntry/Data.php");
 
 echo("\t TEST OF USER_DAO \n\n");
 //creating DAO object
@@ -33,7 +35,7 @@ $User_dao->delete($new_user1);
 print_r($User_dao->findAll());  // We print the content of the database
 
 
-echo("\n\n\n\t TEST OF USER_DAO \n\n");
+echo("\n\n\n\t TEST OF ACTIVITY_DAO \n\n");
 
 //Let's test the ActivityDAO
 
@@ -44,10 +46,10 @@ $Activity_dao->deleteAll();
 // creating new activities and initilizing its values
 echo("Inserting line in database : \n");
 $new_activity1 = new Activity();
-$new_activity1->init("0001", "22:04:2020", "Course a pied", "0006"); //We init the user with multiples parameters
+$new_activity1->init("0001", "22:04:2020", "Course a pied", "0006"); //We init the activity with multiples parameters
 
 $new_activity2 = new Activity();
-$new_activity2->init("0002", "22:05:2019", "Natation", "0006"); //We init the user with multiples parameters
+$new_activity2->init("0002", "22:05:2019", "Natation", "0006"); //We init the activity with multiples parameters
 
 $Activity_dao->insert($new_activity1);
 $Activity_dao->insert($new_activity2); // Inserting the new activities in the database
@@ -63,6 +65,39 @@ print_r($Activity_dao->findAll());  // We print the content of the database
 echo("Removing line : \n");
 $Activity_dao->delete($new_activity1);
 print_r($Activity_dao->findAll());  // We print the content of the database
+
+
+
+echo("\n\n\n\t TEST OF ACTIVITYENTRY_DAO \n\n");
+
+//Let's test the ActivityEntryDAO
+
+//creating DAO object
+$activityEntry_dao = ActivityEntryDAO::getInstance();
+$activityEntry_dao->deleteAll();
+
+// creating new activitie data and initilizing its values
+echo("Inserting line in database : \n");
+$new_activityEntry1 = new Data();
+$new_activityEntry1->init("0100", "13:00:00", "99", "47.644795", "-2.776605", "18", "002"); //We init the activityEntry with multiples parameters
+
+$new_activityEntry2 = new Data();
+$new_activityEntry2->init("0200", "13:00:05", "100", "47.646870", "-2.778911", "18", "002"); //We init the actityEntry with multiples parameters
+
+$activityEntry_dao->insert($new_activityEntry1);
+$activityEntry_dao->insert($new_activityEntry2); // Inserting the new activitieEntry in the database
+print_r($activityEntry_dao->findAll());  // We print the content of the database
+
+// updating the activityEntry1 php object, and updating the database
+echo("Updating line : \n");
+$new_activityEntry1->init("0100", "13:00:00", "80", "47.644795", "-2.776605", "18", "002"); //We modify the activityEntry description
+$activityEntry_dao->update($new_activityEntry1);
+print_r($activityEntry_dao->findAll());  // We print the content of the database
+
+// removing activityEntry1 from the database
+echo("Removing line : \n");
+$activityEntry_dao->delete($new_activityEntry1);
+print_r($activityEntry_dao->findAll());  // We print the content of the database
 
 
 
