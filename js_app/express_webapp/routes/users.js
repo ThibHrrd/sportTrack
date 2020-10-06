adb8d98fee4b8fc1077ba55c5d82758a18136dff
Leo <leo951206@gmail.com>
2020-10-06 02:45:30 -0700

    ONESTDESMONSTRESTHIBZZZZZ

--- js_app/express_webapp/routes/users.js
+++ js_app/express_webapp/routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
+
 var router = express.Router();
-var user_dao = require(sport-track-db').user_dao;
+var user_dao = require('sport-track-db').user_dao;
+
+
 router.get('/', function(req, res, next) {
   user_dao.findAll(function(err, rows) {
     if(err != null){
